09bec8354585ad9df2e939986b8db81f3d99f4e2
nuumenyajelapki <33455940+nuumenyajelapki@users.noreply.github.com>
2020-08-24 20:41:39 +0300

    CotoPloy

---
+++ CotoTask/precache-manifest.6c4ee4d764fab5e4c247dd775d70dab5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a887ffdf1aed47e91e843a5295ac3f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1c866b0e4ac8663f2c9",
+    "url": "/static/css/main.3cc3bfaa.chunk.css"
+  },
+  {
+    "revision": "42edfcce3675b616a316",
+    "url": "/static/js/2.5dcfbd75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5dcfbd75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1c866b0e4ac8663f2c9",
+    "url": "/static/js/main.2bede460.chunk.js"
+  },
+  {
+    "revision": "61324213f0711bcbe172",
+    "url": "/static/js/runtime-main.7b8dbe22.js"
+  },
+  {
+    "revision": "7c5edfeeecfbe020fc4c336f811065e3",
+    "url": "/static/media/bg.7c5edfee.png"
+  },
+  {
+    "revision": "a1966c3a00d944131fd52baa6a9b37fa",
+    "url": "/static/media/cat.a1966c3a.png"
+  }
+]);
